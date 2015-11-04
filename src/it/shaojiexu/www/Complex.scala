2da7af5f5d28074a07fb83780d21f151e84e6232
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-11-04 11:59:15 +0100

    override the toString method in scala class

--- src/it/shaojiexu/www/Complex.scala
+++ src/it/shaojiexu/www/Complex.scala
@@ -7,4 +7,6 @@ class Complex(real:Double, imaginary:Double) {
   def re = real
   def im = imaginary
   
+  override def toString() = String.format("[ real : %s, imaginary : %s]", String.valueOf(real), String.valueOf(im))
+  
 }
--- src/it/shaojiexu/www/ComplexCompanion.scala
+++ src/it/shaojiexu/www/ComplexCompanion.scala
@@ -6,6 +6,7 @@ object ComplexCompanion {
        
        val complex = new Complex(1.3,4.5)
        println("imaginary part : " + complex.im)
+       println(complex.toString())
      }
      
 }
