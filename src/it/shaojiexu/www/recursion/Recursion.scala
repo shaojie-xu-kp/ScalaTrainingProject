406753b6cad654a3161a1beb4033229ea49a3455
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-11-09 09:35:23 +0100

    polish a bit the code

--- src/it/shaojiexu/www/recursion/Recursion.scala
+++ src/it/shaojiexu/www/recursion/Recursion.scala
@@ -6,18 +6,15 @@ object Recursion {
   def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
   
   //non tail recursion, stack problem
-  def factorial(n: Int): Int = if (n == 0) 1 else n * factorial(n - 1)
+  def factorial(n: Long): Long = if (n == 0) 1 else n * factorial(n - 1)
   
   //tail recursion version of factorial, stack safe
-  def tailRecursiveFactorial(n: Int, m: Int) : Int = {
-    if(n == 1) m
-    else tailRecursiveFactorial(n-1, m*n);
-  }
+  def tailRecursiveFactorial(n: Long, m: Long) : Long = if(n == 1) m else tailRecursiveFactorial(n-1, m*n)
   
    def main(args:Array[String]) {
      println(gcd(4,10))
-     println(factorial(5))
-     println(tailRecursiveFactorial(5,1))
+     println(factorial(10))
+     println(tailRecursiveFactorial(10,1))
   }
 
 }
